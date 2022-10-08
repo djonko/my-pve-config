7568f585e3a6cbf26d8a693bf56e4821c8aea81d
prevost <prevostdjonko@gmail.com>
2022-10-08 18:41:02 -0400

    fix

--- scripts/create-cloud-image.sh
+++ scripts/create-cloud-image.sh
@@ -27,12 +27,13 @@ echo "Step $NXT"
 TEMPL_NAME="ubuntu2204-cloud"
 VMID="9000"
 MEM="512"
-DISK_SIZE="3G"
 DISK_STOR="local"
 NET_BRIDGE="vmbr0"
 SSH_PUB="$HOME_USER/.ssh/id_ed25519.pub"
+MY_DNS="pihole.ui24.mywire.com"
+MY_DOMAIN="ui24.mywire.com"
 
-qm create $VMID --name $TEMPL_NAME --memory $MEM --net0 virtio,bridge=$NET_BRIDGE
+qm create $VMID --name $TEMPL_NAME --memory $MEM --net0 virtio,bridge=$NET_BRIDGE --localtime true --nameserver $MY_DNS --searchdomain $MY_DOMAIN
 qm importdisk $VMID $IMG_NAME $DISK_STOR
 qm set $VMID --scsihw virtio-scsi-pci --scsi0 $DISK_STOR:vm-$VMID-disk-0
 qm set $VMID --ide2 $DISK_STOR:cloudinit
@@ -41,7 +42,7 @@ qm set $VMID --serial0 socket --vga serial0
 qm set $VMID --ipconfig0 ip=dhcp
 qm set $VMID --sshkey $SSH_PUB
 qm set $VMID --agent enabled=1
-qm resize $VMID scsi0 $DISK_SIZE
+#qm resize $VMID scsi0 $DISK_SIZE
 qm template $VMID
 rm $IMG_NAME
 
