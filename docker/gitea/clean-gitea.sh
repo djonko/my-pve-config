3cc64ca50eb5247264ec6b686652a002f1d58770
djonko <prevostdjonko@yahoo.fr>
2022-10-16 23:08:12 -0400

    start gitea

--- docker/gitea/clean-gitea.sh
+++ docker/gitea/clean-gitea.sh
@@ -1,1 +1,6 @@
 sudo rm -rf /opt/data-adm-docker/volumes/gitea/gitea-data/* && sudo rm -rf /opt/data-adm-docker/volumes/gitea/postgres/*
+
+# docker compose  --env-file /opt/data-etc/gitea/config.env up
+xsmtpsib-be03ef01528d9754d0b65a988751d80ee1a535f59d2bac528e866eec755e4904-EW6VLqPrfgAHZ7Qj
+
+LNKvU4S4oeP23dN
