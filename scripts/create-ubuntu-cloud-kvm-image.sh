c2fcaa97513654e3f5b2529b054b6f8869207681
djonko <prevostdjonko@gmail.com>
2023-08-19 23:45:46 -0400

    fix

--- scripts/create-ubuntu-cloud-kvm-image.sh
+++ scripts/create-ubuntu-cloud-kvm-image.sh
@@ -3,13 +3,14 @@
 ## > ./create-debian-cloud-image.sh bobdi 10G 2048
 
 NXT=$((0+1))
-HD_SIZE=$2
-USER_HOME_NAME=$1
-MEM=$3
-HOME_USER="/home/$USER_HOME_NAME"
-WORK_DIR="$HOME_USER/tmp/cloudImage"
+HD_SIZE=$1
+
+MEM=$2
+HOME_USER="/tmp"
+WORK_DIR="$HOME_USER/cloudImage"
 DOWNLOAD_URL="https://cloud-images.ubuntu.com/lunar/current/lunar-server-cloudimg-amd64-disk-kvm.img"
-IMG_NAME="lunar-server-kvm.qcow2"
+DOWNLOAD_URL="https://cloud-images.ubuntu.com/lunar/current/lunar-server-cloudimg-amd64.img"
+IMG_NAME="lunar-server-amd64.qcow2"
 ## Step 1: Download the image
 echo "Step $NXT"
 mkdir -p "$WORK_DIR"
@@ -24,7 +25,10 @@ NXT=$((NXT+1))
 echo "Step $NXT"
 apt update
 apt install -y libguestfs-tools
-virt-customize --install qemu-guest-agent -a $IMG_NAME
+virt-customize -a $IMG_NAME --update
+virt-customize --install qemu-guest-agent,git,htop -a $IMG_NAME
+virt-customize -a $IMG_NAME --timezone "America/Montreal"
+
 NXT=$((NXT+1))
 echo "Step $NXT"
 
@@ -33,22 +37,22 @@ TEMPL_NAME="ubuntu-cloud"
 VMID="9801"
 
 DISK_SIZE=$HD_SIZE
-DISK_STOR="zfsa"
+DISK_STOR="pve-vms"
 NET_BRIDGE="vmbr0"
 SSH_PUB="$HOME_USER/.ssh/id_ed25519.pub"
-MY_DNS="192.168.20.2"
-MY_DOMAIN="ui24.mywire.com"
+MY_DNS="192.168.30.1"
+MY_DOMAIN="sp1.theworkpc.com"
 
 qm create $VMID --name $TEMPL_NAME --memory $MEM --net0 virtio,bridge=$NET_BRIDGE --localtime true --nameserver $MY_DNS --searchdomain $MY_DOMAIN
 qm importdisk $VMID $IMG_NAME $DISK_STOR
-qm set $VMID --scsihw virtio-scsi-pci --scsi0 $DISK_STOR:vm-$VMID-disk-0
-qm set $VMID --ide2 $DISK_STOR:cloudinit --boot c --bootdisk scsi0 --serial0 socket --vga serial0
+qm set $VMID --scsihw virtio-scsi-pci --scsi0 ${DISK_STOR}:${VMID}/vm-${VMID}-disk-0
+qm set $VMID --ide2 ${DISK_STOR}:cloudinit --boot c --bootdisk scsi0 --serial0 socket --vga serial0
 qm set $VMID --ipconfig0 ip=dhcp
-qm set $VMID --sshkey "$SSH_PUB"
+#qm set $VMID --sshkey "$SSH_PUB"
 qm set $VMID --agent enabled=1
 qm resize $VMID scsi0 "$DISK_SIZE"
 qm template $VMID
-rm $IMG_NAME
+#rm $IMG_NAME
 
 NXT=$((NXT+1))
 echo "Step $NXT:  END"
