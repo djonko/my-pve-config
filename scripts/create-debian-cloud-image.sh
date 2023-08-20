e6cc773b153cdd7784995d5d61ee8b77569f5e75
djonko <prevostdjonko@gmail.com>
2023-08-19 21:57:54 -0400

    fix

--- scripts/create-debian-cloud-image.sh
+++ scripts/create-debian-cloud-image.sh
@@ -50,7 +50,7 @@ qm set $VMID --ipconfig0 ip=dhcp
 qm set $VMID --agent enabled=1
 qm resize $VMID scsi0 "$DISK_SIZE"
 qm template $VMID
-rm $IMG_NAME
+#rm $IMG_NAME
 
 NXT=$((NXT+1))
 echo "Step $NXT:  END"
