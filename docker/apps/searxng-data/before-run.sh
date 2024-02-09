7d4c64540608f1ca3b8aebc0be3798723eb5bb67
djonko <prevostdjonko@gmail.com>
2024-02-08 20:18:30 -0500

    fix docker-ctl

--- docker/apps/searxng-data/before-run.sh
+++ docker/apps/searxng-data/before-run.sh
@@ -1,4 +1,4 @@
-cp searxng/settings.yml  /opt/data-adm-docker/volumes/searxng/data
-sed -i "s|ultrasecretkey|$(openssl rand -hex 32)|g" /opt/data-adm-docker/volumes/searxng/data/settings.yml
-docker compose  --env-file /opt/data-adm-docker/volumes/searxng/.env  down -v
-docker compose --env-file /opt/data-adm-docker/volumes/searxng/.env up
+cp searxng/settings.yml  /opt/data-docker/searxng/data
+sed -i "s|ultrasecretkey|$(openssl rand -hex 32)|g" /opt/data-docker/searxng/data/settings.yml
+docker compose  --env-file /opt/data-docker/searxng/.env  down -v
+docker compose --env-file /opt/data-docker/searxng/.env up
