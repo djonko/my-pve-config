f1fb8bffa1b78e2f98a22e2e6e92a18c86ba5ce3
djonko <prevostdjonko@gmail.com>
2024-02-08 19:55:42 -0500

    fix docker-ctl

--- docker/apps/offen-docker-bkp/run.sh
+++ docker/apps/offen-docker-bkp/run.sh
@@ -1,1 +1,1 @@
-  docker exec backup /bin/sh -c 'set -a; source /etc/dockervolumebackup/conf.d/apps.env; set +a && backup'
+docker exec backup /bin/sh -c 'set -a; source /etc/dockervolumebackup/conf.d/apps.env; set +a && backup'
