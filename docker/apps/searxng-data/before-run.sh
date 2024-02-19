c003077e25e20780d29df94da4bf902709f787ce
djonko <prevostdjonko@gmail.com>
2024-02-19 18:57:24 -0500

    fix apps

--- docker/apps/searxng-data/before-run.sh
+++ docker/apps/searxng-data/before-run.sh
@@ -2,3 +2,4 @@ cp searxng/settings.yml  /opt/data-docker/searxng/data
 sed -i "s|ultrasecretkey|$(openssl rand -hex 32)|g" /opt/data-docker/searxng/data/settings.yml
 docker compose  --env-file /opt/data-docker/searxng/.env  down -v
 docker compose --env-file /opt/data-docker/searxng/.env up -d
+
