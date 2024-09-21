8a77903f6b870675c67d1d798482a9a59b7bc3d1
djonko <prevostdjonko@yahoo.fr>
2024-09-20 22:44:35 -0400

    fix mariadb

--- scripts/db-restore.sh
+++ scripts/db-restore.sh
@@ -1,2 +1,5 @@
 psql -U $(< /run/secrets/POSTGRES_USER) $(< /run/secrets/POSTGRES_DB) < dump_name.sql
 mariadb -u root --password=$(< /run/secrets/MYSQL_ROOT_PASSWORD) < dump_name.sql
+
+mysql -u  –p  < backup-nginx20240306.sql
+mariadb -u root --password=$(< /run/secrets/MYSQL_ROOT_PASSWORD) < backup-nginx20240306.sql
