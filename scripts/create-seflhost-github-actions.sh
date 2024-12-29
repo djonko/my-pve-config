cd2d4d1a301223d9b1b3b6bd849cec178db2ff4b
djonko <prevostdjonko@gmail.com>
2024-12-29 02:05:22 -0500

    add action

--- scripts/create-seflhost-github-actions.sh
+++ scripts/create-seflhost-github-actions.sh
@@ -1,5 +1,5 @@
 # Create a folder
-$ mkdir actions-runner && cd actions-runner
+$ mkdir ~/.actions-runner && cd ~/.actions-runner
 # Download the latest runner package
 $ curl -o actions-runner-linux-x64-2.321.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.321.0/actions-runner-linux-x64-2.321.0.tar.gz
 # Optional: Validate the hash
